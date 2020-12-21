b31ace54c437912c6f9c431f8b6e7090f0f28156
George Vasiliadis <gvasiliadis2@gmail.com>
2020-12-21 16:05:07 +0200

    Important documentation and code clean-up
    - Docstrings were added for every class/function.
    - Player.move was refactored

--- agar.py
+++ agar.py
@@ -1,22 +1,39 @@
 import pygame,random,math
 
-# General Settings
-PLAYER_COLORS = [(37,7,255),(35,183,253),(48,254,241),(19,79,251),(255,7,230),(255,7,23),(6,254,13)]
-CELL_COLORS = [(80,252,54),(36,244,255),(243,31,46),(4,39,243),(254,6,178),(255,211,7),(216,6,254),(145,255,7),(7,255,182),(255,6,86),(147,7,255)]
+# Color Definitions
+PLAYER_COLORS = [
+    (37,7,255),
+    (35,183,253),
+    (48,254,241),
+    (19,79,251),
+    (255,7,230),
+    (255,7,23),
+    (6,254,13)]
+
+CELL_COLORS = [
+    (80,252,54),
+    (36,244,255),
+    (243,31,46),
+    (4,39,243),
+    (254,6,178),
+    (255,211,7),
+    (216,6,254),
+    (145,255,7),
+    (7,255,182),
+    (255,6,86),
+    (147,7,255)]
+
 VIRUS_COLORS = [(66,254,71)]
+
+# Dimension Definitions
 SCREEN_WIDTH, SCREEN_HEIGHT = (800,500)
 PLATFORM_WIDTH, PLATFORM_HEIGHT = (2000,2000)
+
+# Other Definitions
 CELLS = list()
 
-# Initializing pygame modules
+# Pygame initialization
 pygame.init()
-MAIN_SURFACE = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
- # Transparent rect for score
-SCOREBOARD_SURFACE = pygame.Surface((95,25),pygame.SRCALPHA)
-# Transparent rect for leaderboard
-LEADERBOARD_SURFACE = pygame.Surface((155,278),pygame.SRCALPHA) 
-SCOREBOARD_SURFACE.fill((50,50,50,80))
-LEADERBOARD_SURFACE.fill((50,50,50,80))
 pygame.display.set_caption("Agar.io")
 clock = pygame.time.Clock()
 try:
@@ -27,12 +44,22 @@ except:
     font = pygame.font.SysFont('Ubuntu',20,True)
     big_font = pygame.font.SysFont('Ubuntu',24,True)
 
-# Blits text to central (global) screen
-def drawText(message,pos,color=(255,255,255)):
-        MAIN_SURFACE.blit(font.render(message,1,color),pos)
+# Surface Definitions
+MAIN_SURFACE = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+SCOREBOARD_SURFACE = pygame.Surface((95,25),pygame.SRCALPHA)
+LEADERBOARD_SURFACE = pygame.Surface((155,278),pygame.SRCALPHA) 
+SCOREBOARD_SURFACE.fill((50,50,50,80))
+LEADERBOARD_SURFACE.fill((50,50,50,80))
 
-# Calculates Euclidean distance
+# Auxiliary Functions
+def drawText(message,pos,color=(255,255,255)):
+    """Blits text to main (global) screen.
+    """
+    MAIN_SURFACE.blit(font.render(message,1,color),pos)
+
 def getDistance(pos1,pos2):
+    """Calculates Euclidean distance between given points.
+    """
     px,py = pos1
     p2x,p2y = pos2
     diffX = math.fabs(px-p2x)
@@ -41,7 +68,8 @@ def getDistance(pos1,pos2):
 
 
 class Camera:
-    """Camera is used to represent the concept of POV."""
+    """Used to represent the concept of POV.
+    """
 
     def __init__(self):
         self.x = 0
@@ -50,8 +78,11 @@ class Camera:
         self.height = SCREEN_HEIGHT
         self.zoom = 0.5
 
-    # Makes sure that the given object will be at the center of player's view. Zooms out/ in as well
+    
     def centre(self,blobOrPos):
+        """Makes sure that the given object will be at the center of player's view. 
+        Zooms is taken into account as well.
+        """
         if(isinstance(blobOrPos,Player)):
             p = blobOrPos
             self.x = (p.startX-(p.x*self.zoom))-p.startX+((SCREEN_WIDTH/2))
@@ -59,29 +90,33 @@ class Camera:
         elif(type(blobOrPos) == tuple):
             self.x,self.y = blobOrPos
 
-
 class Player:
-    """Player is used to represent the concept of the player."""
+    """Used to represent the concept of a player.
+    """
+    
     def __init__(self,surface,name = ""):
         self.startX = self.x = random.randint(100,400)
         self.startY = self.y = random.randint(100,400)
         self.mass = 20
         self.speed = 4
         self.surface = surface
-
         # Initialize player with a random color
         self.color = PLAYER_COLORS[random.randint(0,len(PLAYER_COLORS)-1)]
         self.name = name
         self.pieces = list()
-        piece = Piece(surface,(self.x,self.y),self.color,self.mass,self.name)
+        piece = Piece(surface,(self.x,self.y),self.color,self.mass,self.name) #not used or needed
 
-    # Should be called before each new blit
     def update(self):
+        """Updates players position and detects cells that can be eaten.
+        Cells than can be  eaten are appropriately removed.
+        """
         self.move()
         self.collisionDetection()
 
-    # Any cell closer than the radius of player will be eaten
     def collisionDetection(self):
+        """Detects cells being inside the radius of current player.
+        Those cells are eaten.
+        """
         for cell in CELLS:
             if(getDistance((cell.x,cell.y),(self.x,self.y)) <= self.mass/2):
                 self.mass+=0.5
@@ -89,6 +124,9 @@ class Player:
 
 
     def move(self):
+        """Updates players current position depending on player's mouse relative position.
+        """
+        
         dX,dY = pygame.mouse.get_pos()
         # Find the angle from the center of the screen to the mouse in radians [-Pi, Pi]
         rotation = math.atan2(dY-(float(SCREEN_HEIGHT)/2),dX-(float(SCREEN_WIDTH)/2))
@@ -110,17 +148,23 @@ class Player:
         self.y = tmpY
 
     def feed(self):
+        """Unsupported feature.
+        """
         pass
 
     def split(self):
+        """Unsupported feature.
+        """
         pass
 
-    # Draw the player
     def draw(self,cam):
+        """Draws a player as an outlined circle.
+        """
         col = self.color
         zoom = cam.zoom
         x = cam.x
         y = cam.y
+        
         # Draw the ouline of the player as a darker, bigger circle
         pygame.draw.circle(self.surface,(col[0]-int(col[0]/3),int(col[1]-col[1]/3),int(col[2]-col[2]/3)),(int(self.x*zoom+x),int(self.y*zoom+y)),int((self.mass/2+3)*zoom))
         # Draw the actual player as a circle
@@ -130,8 +174,9 @@ class Player:
             # Draw player's name
             drawText(self.name, (self.x*cam.zoom+cam.x-int(fw/2),self.y*cam.zoom+cam.y-int(fh/2)),(50,50,50))
 
-# Totally unused class
 class Piece:
+    """Unsupported feature.
+    """
     def __init__(self,surface,pos,color,mass,name,transition=False):
         self.x,self.y = pos
         self.mass = mass
@@ -146,9 +191,12 @@ class Piece:
         if(self.splitting):
             pass
 
-# Semantically, this is a parent class of player
-# Represents the smallest quantities of mass the player eats
-class Cell:
+
+class Cell: # Semantically, this is a parent class of player
+    """Used to represent the fundamental entity of game.
+    A cell can be considered as a quantom of mass.
+    It can be eaten by other entities.
+    """
     def __init__(self,surface):
         self.x = random.randint(20,1980)
         self.y = random.randint(20,1980)
@@ -157,27 +205,37 @@ class Cell:
         self.color = CELL_COLORS[random.randint(0,len(CELL_COLORS)-1)]
 
     def draw(self,cam):
+        """Draws a cell as a simple circle.
+        """
         pygame.draw.circle(self.surface,self.color,(int((self.x*cam.zoom+cam.x)),int(self.y*cam.zoom+cam.y)),int(self.mass*cam.zoom))
 
-# Populates the global cell-list with randomly placed cells
 def spawn_cells(numOfCells):
+    """Populates the global cell-list with randomly placed cells.
+    """
     for i in range(numOfCells):
         cell = Cell(MAIN_SURFACE)
         CELLS.append(cell)
 
-# Draws the background grid on every call
+
 def draw_grid():
+    """Draws the background grid on main surface.
+    """
+
+    # A grid is a set of horizontal and prependicular lines
     for i in range(0,2001,25):
         pygame.draw.line(MAIN_SURFACE,(230,240,240),(0+camera.x,i*camera.zoom+camera.y),(2001*camera.zoom+camera.x,i*camera.zoom+camera.y),3)
         pygame.draw.line(MAIN_SURFACE,(230,240,240),(i*camera.zoom+camera.x,0+camera.y),(i*camera.zoom+camera.x,2001*camera.zoom+camera.y),3)
 
+
+# Initialize essential entities
 camera = Camera()
-# Main player
 blob = Player(MAIN_SURFACE,"Viliami")
 spawn_cells(2000)
 
-# Draws the Heads-Up Display on screen.
+
 def draw_HUD():
+    """Draws the Heads-Up Display the main surface.
+    """
     w,h = font.size("Score: "+str(int(blob.mass*2))+" ")
     MAIN_SURFACE.blit(pygame.transform.scale(SCOREBOARD_SURFACE,(w,h)),(8,SCREEN_HEIGHT-30))
     MAIN_SURFACE.blit(LEADERBOARD_SURFACE,(SCREEN_WIDTH-160,15))
@@ -197,30 +255,24 @@ def draw_HUD():
     else:
         drawText("10. Viliami",(SCREEN_WIDTH-157,20+25*10),(210,0,0))
 
-# Main loop
+# Game main loop
 while(True):
-    # Set FPS
+    
     clock.tick(70)
-
-    # Check for events
     for e in pygame.event.get():
         if(e.type == pygame.KEYDOWN):
-            # The player pressed 'esc'
             if(e.key == pygame.K_ESCAPE):
                 pygame.quit()
                 quit()
-            # The player pressed 'space'
             if(e.key == pygame.K_SPACE):
                 blob.split()
-            # The player pressed 'W'
             if(e.key == pygame.K_w):
                 blob.feed()
-        # The player clicked the 'X' button
         if(e.type == pygame.QUIT):
             pygame.quit()
             quit()
 
-    # Check what player eats and set his/her new position
+
     blob.update()
     # Update camera zoom. Is this supposed to be here?
     camera.zoom = 100/(blob.mass)+0.3
